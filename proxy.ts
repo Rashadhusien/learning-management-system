9d2c11f88768edd8e6d69d7a5cd6f8e1ca1d4f85
rashad <rashadhusein440@gmail.com>
2026-03-05 14:52:51 +0200

    convert middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { ROUTES } from "./constants/routes";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const session = await auth();
 
